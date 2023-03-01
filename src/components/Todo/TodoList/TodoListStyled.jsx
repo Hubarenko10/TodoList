2d12182a3e1a90a7daac574e08579999f909064c
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-03-01 16:52:41 +0200

    edits

--- src/components/Todo/TodoList/TodoListStyled.jsx
+++ src/components/Todo/TodoList/TodoListStyled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const BoxList = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 10px;
+  margin-top: 20px;
   margin-left: 20px;
   width: 30%;
 `;
